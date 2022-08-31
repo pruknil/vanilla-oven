209c5fe43f184ce2a6400f22ba58c6fa57f054c2
macbook <pruknil@gmail.com>
2022-08-31 21:12:10 +0700

    add nativebase

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React from "react";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+} from "native-base";
+import NativeBaseIcon from "./components/NativeBaseIcon";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useFocusEffect } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MapScreen from './src/screens/Map';
 import AccountScreen from './src/screens/Account';
-import { StyleSheet, Text, View, Dimensions, Button, Image } from 'react-native';
+import { Platform } from "react-native";
+
+
+
+
+
 function HomeScreen({ navigation }) {
   useFocusEffect(
     React.useCallback(() => {
@@ -19,18 +38,43 @@ function HomeScreen({ navigation }) {
     }, [])
   );
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => {
-          navigation.navigate('Settings', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          });
-        }}
-      />
-    </View>
+    <NativeBaseProvider>
+      <Center
+        _dark={{ bg: "blueGray.900" }}
+        _light={{ bg: "blueGray.50" }}
+        px={4}
+        flex={1}
+      >
+        <VStack space={5} alignItems="center">
+          <NativeBaseIcon />
+          <Heading size="lg">Welcome to NativeBase</Heading>
+          <HStack space={2} alignItems="center">
+            <Text>Edit</Text>
+            <Box
+              _web={{
+                _text: {
+                  fontFamily: "monospace",
+                  fontSize: "sm",
+                },
+              }}
+              px={2}
+              py={1}
+              _dark={{ bg: "blueGray.800" }}
+              _light={{ bg: "blueGray.200" }}
+            >
+              App.js
+            </Box>
+            <Text>and save to reload.</Text>
+          </HStack>
+          <Link href="https://docs.nativebase.io" isExternal>
+            <Text color="primary.500" underline fontSize={"xl"}>
+              Learn NativeBase
+            </Text>
+          </Link>
+          <ToggleDarkMode />
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
@@ -47,9 +91,17 @@ function SettingsScreen() {
     }, [])
   );
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
+    <NativeBaseProvider>
+    <Center
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+      flex={1}
+    >
+       <Text>Settings!</Text>
+      
+      </Center></NativeBaseProvider>
+
   );
 }
 
@@ -97,10 +149,46 @@ function MyTabs() {
   );
 }
 
+
+
+
+
+
+
+
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
+    <MyTabs />
+  </NavigationContainer>
+
+  );
+}
+
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light"}
+        onToggle={toggleColorMode}
+        aria-label={
+          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+        }
+      />
+      <Text>Light</Text>
+    </HStack>
   );
 }
