c3aef115234e3f9a7cf5ed03c9c9d3be9fecb639
pruknil <pruknil@gmail.com>
2023-05-06 14:44:47 +0700

    init

--- App.js
+++ App.js
@@ -1,3 +1,120 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Icon} from 'react-native-elements';
+import Signin from './src/screens/Signin';
+import Tab1 from './src/screens/Tab1';
+import Tab2 from './src/screens/Tab2';
+import Tab3 from './src/screens/Tab3';
+import {Provider as AuthProvider} from './src/context/AuthContext.js';
+import {Context as AuthContext} from './src/context/AuthContext';
+
+const AuthStack = createStackNavigator();
+function AuthFlow() {
+    return (
+        <AuthStack.Navigator>
+            <AuthStack.Screen
+                options={{headerShown: false}}
+                name="Signin"
+                component={Signin}
+            />
+            <AuthStack.Screen
+                options={{headerShown: false}}
+                name="Signup"
+                component={Signin}
+            />
+        </AuthStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+function HomeFlow() {
+    return (
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName;
+
+                    switch (route.name) {
+                        case 'Tab1':
+                            iconName = focused
+                                ? 'ios-checkbox'
+                                : 'ios-checkbox-outline';
+                            break;
+                        case 'Tab2':
+                            iconName = focused
+                                ? 'ios-add-circle'
+                                : 'ios-add-circle-outline';
+                            break;
+                        case 'Tab3':
+                            iconName = focused
+                                ? 'ios-information-circle'
+                                : 'ios-information-circle-outline';
+                            break;
+                    }
+
+                    // You can return any component that you like here!
+                    return (
+                        <Icon name={iconName} type="ionicon" size={size} color={color} />
+                    );
+                },
+                "tabBarActiveTintColor": "tomato",
+                "tabBarInactiveTintColor": "gray",
+                "tabBarStyle": [
+                    {
+                        "display": "flex"
+                    },
+                    null
+                ]
+
+            })}
+            >
+            <Tab.Screen name="Tab1" component={Tab1} />
+            <Tab.Screen name="Tab2" component={Tab2} />
+            <Tab.Screen name="Tab3" component={Tab3} />
+        </Tab.Navigator>
+    );
+}
+
+const Stack = createStackNavigator();
+function App() {
+    const {state} = React.useContext(AuthContext);
+    console.log(state);
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {state.token === null ? (
+                    <>
+                        <Stack.Screen
+                            options={{headerShown: false}}
+                            name="Auth"
+                            component={AuthFlow}
+                        />
+                    </>
+                ) : (
+                    <Stack.Screen
+                        options={{headerShown: false}}
+                        name="Home"
+                        component={HomeFlow}
+                    />
+                )}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default () => {
+    return (
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    );
+};
+
+
+
+/*
 
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from "./src/screens/HomeScreen";
@@ -44,3 +161,4 @@ export default function App() {
 }
 
 
+*/
