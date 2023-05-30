0505c81cc9cc9384c9156f1c6aea7545229efaee
macbook <pruknil@gmail.com>
2023-05-30 11:35:12 +0700

    Add Camera

--- src/screens/tabs/MyCamera.js
+++ src/screens/tabs/MyCamera.js
@@ -1,36 +1,59 @@
 import React, {useContext, useEffect, useRef, useState} from 'react';
-import {StyleSheet, ScrollView, View,TouchableOpacity} from 'react-native';
+import {StyleSheet, View,TouchableOpacity} from 'react-native';
 import {Context as AuthContext} from "../../context/AuthContext";
 import {Text,useTheme} from "@rneui/themed";
 import { Camera, CameraType } from 'expo-camera';
-import { CameraFullScreen } from './CameraFullScreen';
 import * as FaceDetector from 'expo-face-detector';
-
+import * as MediaLibrary from 'expo-media-library';
 const MyCamera = ({navigation}) => {
     const {state} = useContext(AuthContext);
     const { theme } = useTheme();
     const [isPreview, setIsPreview] = useState(false);
     const [type, setType] = useState(CameraType.back);
     const [permission, requestPermission] = Camera.useCameraPermissions();
-
+    const [status, requestPermissionMedia] = MediaLibrary.usePermissions();
+    const cameraRef = useRef(null)
     if (requestPermission === null) {
         return <View />;
     }
-
+    if (status === null) {
+        requestPermissionMedia();
+    }
     if (requestPermission === false) {
         return <Text>No access to camera</Text>;
     }
     function toggleCameraType() {
         setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
     }
+    function takePicture(){
+        onSaveImageAsync().then(r => {})
+    }
+
+    const onSaveImageAsync = async () => {
+        try {
+            if (!cameraRef) return
+            const photo = await cameraRef.current.takePictureAsync();
+            await MediaLibrary.saveToLibraryAsync(photo.uri);
+            if (photo) {
+                alert("Saved!");
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     useEffect(() => {
 
     }, []);
 
     return (
-        <View style={{ flex: 1 }}>
-            <CameraFullScreen type={type}
-                              onFacesDetected={handleFacesDetected}
+        <View style={{ flex: 1 }} >
+            <Camera
+                style={{ flex: 1 }}
+                type={type}
+                useCamera2Api={true}
+                ratio={"16:9"}  ref = {cameraRef}
+                onFacesDetected={handleFacesDetected}
                               faceDetectorSettings={{
                                   mode: FaceDetector.FaceDetectorMode.fast,
                                   detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
@@ -38,23 +61,49 @@ const MyCamera = ({navigation}) => {
                                   minDetectionInterval: 100,
                                   tracking: true,
                               }}>
+
+
                 <View
                     style={{
-                        flex: 1,
-                        backgroundColor: 'transparent',
+                        position: 'absolute',
+                        bottom: 0,
                         flexDirection: 'row',
-                    }}>
-                    <TouchableOpacity
+                        flex: 1,
+                        width: '100%',
+                        padding: 20,
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <View
                         style={{
-                            flex: 0.1,
-                            alignSelf: 'flex-end',
-                            alignItems: 'center',
+                            alignSelf: 'center',
+                            flex: 1,
+                            flexDirection: 'column',
+                            alignItems: 'center'
                         }}
-                        onPress={toggleCameraType}>
-                        <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-                    </TouchableOpacity>
+                    >
+                        <TouchableOpacity
+                            style={{
+                                //flex: 1,
+                                alignSelf: 'flex-start',
+                                alignItems: 'left',
+                            }}
+                            onPress={toggleCameraType}>
+                            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={takePicture}
+                            style={{
+                                width: 70,
+                                height: 70,
+                                bottom: 0,
+                                borderRadius: 50,
+                                backgroundColor: '#fff'
+                            }}
+                        />
+                    </View>
                 </View>
-            </CameraFullScreen>
+            </Camera>
         </View>
     );
 };
